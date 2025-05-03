1cd422d028a24f9e1b94699424ecf69388d2778f
Jean Martins <jeanhenriq@gmail.com>
2025-05-02 22:38:48 -0300

    Meu primeiro commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Jean Henrique Miranda Martins")
+
+# Variáveis
+idade = 35
+nome = "Jean"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Listas
+lista = [1,2,3,4,5]
+
